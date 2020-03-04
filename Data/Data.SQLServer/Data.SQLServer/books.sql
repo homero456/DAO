ec5f77090c5451fdb4ba913d9d02fc42e9acc6be
vfabian <vfabian@hotmail.com>
2020-03-04 14:47:49 -0500

    ok

--- Data/Data.SQLServer/Data.SQLServer/books.sql
+++ Data/Data.SQLServer/Data.SQLServer/books.sql
@@ -33,4 +33,30 @@ AS
    
    if @@rowcount <> 1 
       raiserror('Invalid Book Id',16,1)
+
+
+
+-------------------------------------------------------------
+BEGIN TRANSACTION;  
+  
+BEGIN TRY  
+    -- Generate a constraint violation error.  
+    DELETE FROM Production.Product  
+    WHERE ProductID = 980;  
+END TRY  
+BEGIN CATCH  
+    SELECT   
+        ERROR_NUMBER() AS ErrorNumber  
+        ,ERROR_SEVERITY() AS ErrorSeverity  
+        ,ERROR_STATE() AS ErrorState  
+        ,ERROR_PROCEDURE() AS ErrorProcedure  
+        ,ERROR_LINE() AS ErrorLine  
+        ,ERROR_MESSAGE() AS ErrorMessage;  
+  
+    IF @@TRANCOUNT > 0  
+        ROLLBACK TRANSACTION;  
+END CATCH;  
+  
+IF @@TRANCOUNT > 0  
+    COMMIT TRANSACTION;  
          
